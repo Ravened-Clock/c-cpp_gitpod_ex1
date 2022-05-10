6136e39273665381ac6e4d5b2800ef749bcce980
J.C <67492778+YellowMarble@users.noreply.github.com>
2022-05-10 09:49:41 +0800

    Update 凹嗚.cpp

--- 凹嗚.cpp
+++ 凹嗚.cpp
@@ -1,12 +1,13 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
 int main(){
   string wtf;
   cin>>wtf;
+  
   for(int i=0;i<wtf.length();i++){
     cout<<"凹嗚*"<<i+1<<endl;
   }
+  
   return 0;
 }
