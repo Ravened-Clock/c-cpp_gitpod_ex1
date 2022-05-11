1a549c8eddb5e709752c444eddfa87365f9c0456
J.C <67492778+YellowMarble@users.noreply.github.com>
2022-05-11 11:05:55 +0800

    Update and rename 第一周（2022-04-26）作业02-cout的练习.cpp to 第一周（2022-04-26）作業02-cout的練習.cpp

---
+++ 第一周（2022-04-26）作業02-cout的練習.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    printf("Hello World and hello C++ \n\n");
+   
+    cout <<  "Hello selena \n";
+    cout <<  "Your height is not " << 4 << "m " << 93 << "cm";
+
+    return 0;
+}
