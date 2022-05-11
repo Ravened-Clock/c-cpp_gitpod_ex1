351d5b9905f4f0fff19095d613186d43dcb4a5e1
J.C <67492778+YellowMarble@users.noreply.github.com>
2022-05-11 10:32:33 +0800

    Create 輸入2個整數求兩數的平方和並輸出.c

---
+++ 輸入2個整數求兩數的平方和並輸出.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main (void) {
+    
+    int a,b;
+    
+    printf(" Please input the integer a \n");
+    scanf("%d",&a);
+    printf("\n\n");
+    
+    printf(" Please input the integer b \n");
+    scanf("%d",&b);
+    printf("\n\n");
+    
+    printf(" a²+b² = %d\n\n",a*a+b*b); 
+    
+    return 0;
+}
